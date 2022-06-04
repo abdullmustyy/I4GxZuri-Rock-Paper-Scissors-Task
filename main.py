0fd83e69a5d472f20a995c4b2788cc1e67ab7a2b
abdullmustyy <abdulmusty03@outlook.com>
2022-06-04 14:08:05 +0100

    Complete Rock-Paper-Scissors Game

---
+++ main.py
@@ -0,0 +1,48 @@
+import random
+
+options = {
+    'R': 'Rock 🤜🏻',
+    'P': 'Paper 🖐🏻',
+    'S': 'Scissors ✌',
+}
+
+options_list = ['R', 'P', 'S']
+
+
+def check_game():
+    if player_choice == computer_choice.lower():
+        print('It is a tie.')
+    elif player_choice == 'r' and computer_choice.lower() == 'p':
+        print("Paper beats Rock. Computer wins.")
+    elif player_choice == 'r' and computer_choice.lower() == 's':
+        print("Rock beat Scissors. Player wins.")
+    elif player_choice == 'p' and computer_choice.lower() == 'r':
+        print("Paper beat Rock. Player wins.")
+    elif player_choice == 'p' and computer_choice.lower() == 's':
+        print("Scissors beats Paper. Computer wins.")
+    elif player_choice == 's' and computer_choice.lower() == 'r':
+        print("Rock beats Scissors. Computer wins.")
+    elif player_choice == 's' and computer_choice.lower() == 'p':
+        print("Scissors beat Paper. Player wins.")
+    else:
+        print('You entered an invalid input.')
+
+
+print("ROCK 🤜🏻 - PARER 🖐🏻 - SCISSORS ✌\n    Welcome to the game of Rock, Paper, Scissors.\n    Input 'r' for Rock,"
+      " 'p' for Paper and 's' for Scissors.\nEnjoy...\n")
+
+game_on = True
+while game_on:
+
+    computer_choice = random.choice(options_list)
+    prompt = input("Press 'y' to start the game and 'n' to stop it: ").lower()
+    print("")
+
+    if prompt == "y":
+        player_choice = input("Rock(r), Paper(p) or Scissors(s)? ").lower()
+        print(f"\nPlayer ({options[player_choice.upper()]}) : CPU ({options[computer_choice]})\n")
+        check_game()
+    elif prompt == "n":
+        game_on = False
+    else:
+        print('Invalid input, Please pass in the correct the input. \n')
